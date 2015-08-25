e4231e0321658ce6259909c374c99ef72d690a0b
Benedict Aluan <benedict.aluan@gmail.com>
2015-08-26 11:40:48 +1200

    Initial Commit

---
+++ C Blog Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  C Blog Reader
+//
+//  Created by Benedict Aluan on 26/08/15.
+//  Copyright (c) 2015 Pencil Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ C Blog Reader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  C Blog Reader
+//
+//  Created by Benedict Aluan on 26/08/15.
+//  Copyright (c) 2015 Pencil Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
